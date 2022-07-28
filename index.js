8d7c3d8eda22c5f9ad9f728b7997d1e18c7c2c33
Camilo <camilo.certuche@gmail.com>
2022-07-27 21:01:50 -0500

    3.13: Phonebook database, step1

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+
+const Person = require("./models/person");
 
 morgan.token("body", (request, response) => JSON.stringify(request.body));
 
@@ -11,31 +14,10 @@ app.use(express.static("build"));
 app.use(morgan(":method :url :status :response-time ms :body"));
 app.use(cors());
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.get("/info", (request, response) => {
