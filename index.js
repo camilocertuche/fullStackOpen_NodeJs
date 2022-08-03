47195a203e893dc25fe8348da6a22b754c32cda1
Camilo <camilo.certuche@gmail.com>
2022-08-02 21:02:50 -0500

    3.18*: Phonebook database step6

--- index.js
+++ index.js
@@ -26,22 +26,28 @@ app.get("/api/persons", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.get("/info", (request, response) => {
-  const total = persons.length;
+app.get("/info", (request, response, next) => {
   const date = new Date();
 
-  response.send(`<p>Phonebook has info for ${total} people</p><p>${date}</p>`);
+  Person.countDocuments({})
+    .then((count) => {
+      response.send(
+        `<p>Phonebook has info for ${count} people</p><p>${date}</p>`
+      );
+    })
+    .catch((error) => next(error));
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 app.delete("/api/persons/:id", (request, response, next) => {
