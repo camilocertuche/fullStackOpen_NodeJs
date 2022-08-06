3036620fb5f897e261e351fa939855771583728b
Camilo <camilo.certuche@gmail.com>
2022-08-06 15:34:15 -0500

    3.20*: Phonebook database, step8

--- models/person.js
+++ models/person.js
@@ -15,7 +15,17 @@ mongoose
 
 const personSchema = new mongoose.Schema({
   name: { type: String, minLength: 3, required: true },
-  number: String,
+  number: {
+    type: String,
+    minLength: 8,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /\d{2,3}-\d{5,}/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+  },
 });
 
 personSchema.set("toJSON", {
